d24124088ff376eb4ea495aec9e6b1e3a7749c12
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-09-28 06:29:56 +0000

    Feat: Migrate `targets/aws` config
    - Migrate code from `targets/aws`. This repo is a spin-off of the
      old `targets/aws` repo. The spin off occured because the same
      aws config has to target both the aws as a vendor and cloudstack.
      To adhere to DRY, the repo has been split in two
    - Migrate code from `targets/aws`. This repo is a spin-off of the
      old `targets/aws` repo. The spin off occured because the same
      aws config has to target both the aws as a vendor and cloudstack.
      To adhere to DRY, the repo has been split in two.

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "cluster_endpoint" {
+  value = module.base.cluster_endpoint
+}
+
+output "cluster_ca_certificate" {
+  value = module.base.cluster_ca_certificate
+}
+
+output "cluster_token" {
+  value = module.base.cluster_token
+}
